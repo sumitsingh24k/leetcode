4ce66359dead2b172d75a2fa9452c132f2322383
SUMIT SINGH <145108589+sumitsingh24k@users.noreply.github.com>
2026-04-18 21:11:51 +0530

    Time: 20 ms (76.06%), Space: 20 MB (31.75%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,19 +1,18 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        row,col=len(grid),len(grid[0])
-        direction=[(0,1),(0,-1),(1,0),(-1,0)]
+        direction=[(0,1),(0,-1),(1,0), (-1,0)]
         heap = [(grid[0][0], 0, 0)]
+        n=len(grid)
         visited=set()
-        visited.add((0,0))
-        depth=0
         while heap:
-            curr_level,r,c=heapq.heappop(heap)
-            if curr_level > depth:
-                depth=curr_level
-            if r==row-1 and c==col-1:
-                return depth
+            value,row,col=heapq.heappop(heap)
+            if row==n-1 and col==n-1:
+                return value 
             for dr,dc in direction:
-                nr,nc=dr+r,dc+c
-                if 0<=nr<row and 0<=nc<col and (nr,nc) not in visited:
+                nr,nc=dr+row,col+dc
+                if 0<=nr<n and 0<=nc<n and (nr,nc) not in visited:
                     visited.add((nr,nc))
-                    heapq.heappush(heap,(grid[nr][nc],nr,nc))
+                    max_value=max(value,grid[nr][nc])
+                    heapq.heappush(heap,(max_value,nr,nc))
+                    
+        return -1
